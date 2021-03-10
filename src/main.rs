d3af0755c90916934395d8f03c67c350acff7747
josh <jo3f@mtmail.mtsu.edu>
2021-03-09 20:44:50 -0600

    thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
+use std::thread;
+use std::time::Duration;
+
+mod tp;
 
 // https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 
+/*
+Request format:
+
+Method Request-URI HTTP-Version CRLF
+headers CRLF
+message-body
+
+Response Format:
+
+HTTP-Version Status-Code Reason-Phrase CRLF
+headers CRLF
+message-body
+
+*/
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = tp::ThreadPool::new(4);
 
     // incoming: returns an iterator that gives a sequence of streams
     //           of type TcpStream. A single stream represents an open
@@ -13,6 +36,33 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get   = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let response_contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, response_contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
